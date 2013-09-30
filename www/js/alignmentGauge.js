6bf52d83da28ce3bf3f5b9931d6a911d1b146edf
Kevin Cocco <kcocco@sproutloop.com>
2013-09-30 15:11:02 -0600

    centerY change all to cap

--- www/js/alignmentGauge.js
+++ www/js/alignmentGauge.js
@@ -4,10 +4,10 @@ function init_alignmentGauge(elementId){
       // Creates canvas 320 × 200 at 10, 50
       paper = Raphael(elementId, 360, 360);
       c = paper.rect(0, 0, 360, 360, 10);
-      centerx = paper.width/2;
-      centery = paper.height/2;
+      centerX = paper.width/2;
+      centerY = paper.height/2;
       outering = 42;
-      radius = centerx - outering;
+      radius = centerX - outering;
       sunAngle = 0;
       ovenAngle = 0;
       ovenwidth = 20;
@@ -71,7 +71,7 @@ function alignmentGauge(refresh,sunAngle,ovenAngle,sunRiseAngle,sunSetAngle,sunR
       var displayOvenAngle = ((ovenAngle+90)%360);
       //console.log("displayOvenAngle " + displayOvenAngle);
       console.log("centerY " + centerY);
-      console.log("centery " + centery);
+
       //var displayOvenAngle = ((ovenAngle)%360);
 
       // refresh = 1(yes draw entire compass) 0(draw only update items compass and sun)
@@ -79,15 +79,15 @@ function alignmentGauge(refresh,sunAngle,ovenAngle,sunRiseAngle,sunSetAngle,sunR
       	  paper.clear() 
 	      // Draw Compass Outer Dial
 	      var compassDial = paper.set();
-	      compassDial.push(paper.circle(centerx, centery, radius+outering).attr({fill:'black'}));
+	      compassDial.push(paper.circle(centerX, centerY, radius+outering).attr({fill:'black'}));
       
 	      // Draw Compass Headings and marks
 	      var fontAttr = {font:'12px Verdana','font-size':heading_font,'text-anchor':'middle',fill:'white',stroke:"white"};
 	      var headingDiff = 20;
-	      compassDial.push(paper.text( centerx, centery+radius+headingDiff, "N").attr(fontAttr));
-	      compassDial.push(paper.text( centerx, centery-radius-headingDiff, "S").attr(fontAttr));
-	      compassDial.push(paper.text( centerx+radius+headingDiff, centery, "W").attr(fontAttr).rotate(90));
-	      compassDial.push(paper.text( centerx-radius-headingDiff, centery, "E").attr(fontAttr).rotate(-90));
+	      compassDial.push(paper.text( centerX, centerY+radius+headingDiff, "N").attr(fontAttr));
+	      compassDial.push(paper.text( centerX, centerY-radius-headingDiff, "S").attr(fontAttr));
+	      compassDial.push(paper.text( centerX+radius+headingDiff, centerY, "W").attr(fontAttr).rotate(90));
+	      compassDial.push(paper.text( centerX-radius-headingDiff, centerY, "E").attr(fontAttr).rotate(-90));
       
 	      var tickLength=22;
 	      var tickLengthSmall=8;
@@ -97,34 +97,34 @@ function alignmentGauge(refresh,sunAngle,ovenAngle,sunRiseAngle,sunSetAngle,sunR
 	      var fontNumberAttr = {font:'12px Verdana','font-size':degrees_font,'text-anchor':'middle',fill:'white',stroke:'white'};
         var degreeDiff = 17
 	      while (tickAngle<360) {
-	        compassDial.rotate(tickAngle+180,centerx,centery);
-	        if (tickAngle%90!=0) {compassDial.push(paper.path([['M',centerx, centery+radius],['L',centerx, centery+radius+tickLength]]).attr(tickAttr))}
-	        compassDial.rotate(10,centerx,centery);
+	        compassDial.rotate(tickAngle+180,centerX,centerY);
+	        if (tickAngle%90!=0) {compassDial.push(paper.path([['M',centerX, centerY+radius],['L',centerX, centerY+radius+tickLength]]).attr(tickAttr))}
+	        compassDial.rotate(10,centerX,centerY);
 	        if ((textAngle-10)%90!=0) {
-	            compassDial.push(paper.text( centerx, centery-radius-degreeDiff, textAngle-10).attr(fontNumberAttr))
-			    compassDial.push(paper.path([['M',centerx, centery+radius],['L',centerx, centery+radius+tickLengthSmall]]).attr(tickAttr))
+	            compassDial.push(paper.text( centerX, centerY-radius-degreeDiff, textAngle-10).attr(fontNumberAttr))
+			    compassDial.push(paper.path([['M',centerX, centerY+radius],['L',centerX, centerY+radius+tickLengthSmall]]).attr(tickAttr))
 			}
             
-	        compassDial.rotate(-(tickAngle+10+180),centerx,centery);
+	        compassDial.rotate(-(tickAngle+10+180),centerX,centerY);
 	        tickAngle = tickAngle + 20;
 	        textAngle = textAngle-20
 	      }
 
 		  // Arc - Pies
 		  var sunArc = paper.set();
-	      var sunArc = drawPie(sunArc, centerx, centery, radius, 180+sunRiseAngle, sunSetAngle-180);
+	      var sunArc = drawPie(sunArc, centerX, centerY, radius, 180+sunRiseAngle, sunSetAngle-180);
 	      sunArc.attr({fill:'yellow'});
 	      var nightArc = paper.set();
-	      var nightArc = drawPie(nightArc, centerx, centery, radius, sunSetAngle-180,180+sunRiseAngle);
+	      var nightArc = drawPie(nightArc, centerX, centerY, radius, sunSetAngle-180,180+sunRiseAngle);
 	      nightArc.attr({fill:'grey'});
 	      // Draw Sun Set Rise text
 	      var fontRiseSetAttr = {font:'12px Verdana','font-size':suntime_font,'text-anchor':'middle',fill:'white',stroke:'white'};
-	      nightArc.rotate(270-sunSetAngle,centerx,centery)
-	      nightArc.push(paper.text( centerx+radius/2+6, centery+8, sunSetTime).attr(fontRiseSetAttr))
-	      nightArc.rotate(-(90-sunSetAngle),centerx,centery)
-		  nightArc.rotate(270-sunRiseAngle,centerx,centery)
-	      nightArc.push(paper.text( centerx-radius/2-6, centery+8, sunRiseTime+" am").attr(fontRiseSetAttr))
-	      nightArc.rotate(-(90-sunRiseAngle),centerx,centery)
+	      nightArc.rotate(270-sunSetAngle,centerX,centerY)
+	      nightArc.push(paper.text( centerX+radius/2+6, centerY+8, sunSetTime).attr(fontRiseSetAttr))
+	      nightArc.rotate(-(90-sunSetAngle),centerX,centerY)
+		  nightArc.rotate(270-sunRiseAngle,centerX,centerY)
+	      nightArc.push(paper.text( centerX-radius/2-6, centerY+8, sunRiseTime+" am").attr(fontRiseSetAttr))
+	      nightArc.rotate(-(90-sunRiseAngle),centerX,centerY)
 	  }
       
       else {	// refresh != 1 ... only refresh arms and oven
@@ -136,23 +136,23 @@ function alignmentGauge(refresh,sunAngle,ovenAngle,sunRiseAngle,sunSetAngle,sunR
 	  window.oven = paper.set();
       
       // Draw Oven Angle Line
-      var temppath = ["M", centerx,centery, "L", Math.cos(displayOvenAngle*(Math.PI/180))*radius+centerx, Math.sin(displayOvenAngle*(Math.PI/180))*radius+centery].join(' ');
+      var temppath = ["M", centerX,centerY, "L", Math.cos(displayOvenAngle*(Math.PI/180))*radius+centerX, Math.sin(displayOvenAngle*(Math.PI/180))*radius+centerY].join(' ');
       window.armsSunCompass.push(paper.path(temppath).attr({'stroke-width':armStrokeWidth,'arrow-end':"classic-long-wide"}));
       
       // Draw Sun Angle Line
-      var temppath = ["M", centerx,centery, "L", Math.cos(displaySunAngle*(Math.PI/180))*radius+centerx, Math.sin(displaySunAngle*(Math.PI/180))*radius+centery].join(' ');
+      var temppath = ["M", centerX,centerY, "L", Math.cos(displaySunAngle*(Math.PI/180))*radius+centerX, Math.sin(displaySunAngle*(Math.PI/180))*radius+centerY].join(' ');
       window.armsSunCompass.push(paper.path(temppath).attr({"opacity":0.9,"stroke-width":armStrokeWidth,"stroke":"orange","stroke-dasharray":"-"}));
 
       // Draw Sun
-      window.armsSunCompass.push(paper.circle(Math.cos(displaySunAngle*(Math.PI/180))*(radius+outering/2)+centerx, Math.sin(displaySunAngle*(Math.PI/180))*(radius+outering/2)+centery,sunDiam).attr({fill:"orange",stroke:"orange",opacity:0.9}));
+      window.armsSunCompass.push(paper.circle(Math.cos(displaySunAngle*(Math.PI/180))*(radius+outering/2)+centerX, Math.sin(displaySunAngle*(Math.PI/180))*(radius+outering/2)+centerY,sunDiam).attr({fill:"orange",stroke:"orange",opacity:0.9}));
 
       // Creates oven
-      window.oven.push(paper.rect(centerx - ovenwidth/2, centery - ovenwidth/2, ovenwidth, ovenwidth, 2 ).attr({fill:'black'}));
-      ovenReflectorPath =["M", centerx - ovenwidth/2, centery + ovenwidth/2, "L", centerx - ovenwidth, centery + ovenwidth, "L", centerx + ovenwidth, centery + ovenwidth, "L", centerx + ovenwidth/2, centery + ovenwidth/2].join(' ');
+      window.oven.push(paper.rect(centerX - ovenwidth/2, centerY - ovenwidth/2, ovenwidth, ovenwidth, 2 ).attr({fill:'black'}));
+      ovenReflectorPath =["M", centerX - ovenwidth/2, centerY + ovenwidth/2, "L", centerX - ovenwidth, centerY + ovenwidth, "L", centerX + ovenwidth, centerY + ovenwidth, "L", centerX + ovenwidth/2, centerY + ovenwidth/2].join(' ');
       window.oven.push(paper.path(ovenReflectorPath).attr({fill:'#edf1f9'}));
       window.oven.attr("stroke", "black");
       window.oven.attr("stroke-width",1);
       // rotate oven
-       window.oven.rotate(displayOvenAngle-90, centerx, centery);      
-      //window.oven.rotate(displayOvenAngle, centerx, centery);      
+       window.oven.rotate(displayOvenAngle-90, centerX, centerY);      
+      //window.oven.rotate(displayOvenAngle, centerX, centerY);      
 }
